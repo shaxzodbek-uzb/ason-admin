16a1d014bd07f503c02e2bd3c68b7019c4c69897
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2020-05-09 14:28:16 +0500

    fix

--- app/Cart.php
+++ app/Cart.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     protected $fillable = ['user_id', 'meta'];
-    public function productsList()
+    public function products()
     {
-      return $this->belongsTo('App\Product', 'meta.products')
+      return $this->belongsTo('App\Product', 'meta.products');
     }
 }
